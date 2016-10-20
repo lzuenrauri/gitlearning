fbb673bcc14331790a35346d4ac52dd71482bce8
Happyplankton <lzuenrauri@users.noreply.github.com>
2016-10-20 10:19:48 +0800

    learnyounode 2 finished

--- program.js
+++ program.js
@@ -1,5 +1,5 @@
 var number = 0;
 for(i = 2; i < process.argv.length; i++) {
-	number += Number(console.log(process.argv[i]));
+	number += Number(process.argv[i]);
 }
-console.log("number");
+console.log(number);
